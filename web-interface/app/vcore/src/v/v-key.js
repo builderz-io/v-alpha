d4a95405247dedc4a59c780c30e383b5dc0eeee9
philipeachille <philipeachille@gmail.com>
2020-06-29 14:09:33 +0200

    Create v-key.js

---
+++ web-interface/app/vcore/src/v/v-key.js
@@ -0,0 +1,29 @@
+const VKey = ( function() { // eslint-disable-line no-unused-vars
+
+  /**
+   * V Core Module to manage API Keys
+   *
+   */
+
+  const apiKeys = {
+    googlePlaces: 'YOUR API KEY HERE',
+    mapBox: 'YOUR API KEY HERE',
+  };
+
+  /* ================== public methods ================== */
+
+  function getApiKey( which ) {
+    return apiKeys[which];
+  }
+
+  /* ====================== export ====================== */
+
+  ( () => {
+    V.getApiKey = getApiKey;
+  } )();
+
+  return {
+    getApiKey: getApiKey,
+  };
+
+} )();
