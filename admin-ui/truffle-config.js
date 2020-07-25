07f36d1058157c28b0fb92b7d44d9c6239f99313
grifma <grifma@protonmail.com>
2020-07-25 14:17:20 +0100

    Deleted contracts, migrations, and tests from admin-ui folder. Truffle config references ../smart-contracts

--- admin-ui/truffle-config.js
+++ admin-ui/truffle-config.js
@@ -1,28 +1,54 @@
-const path = require("path");
-const mnemonic = require("./mnemonic");
 const HDWalletProvider = require("truffle-hdwallet-provider");
+const private = require("./private");
+const path = require("path");
 
 module.exports = {
   contracts_directory: "../smart-contract/contracts",
+  test_directory: "../smart-contract/test",
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
-    develop: {
-      // host: "127.0.0.1",
-      // port: 8545,
-      // network_id: "*"
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      websockets: true,
+    },
+    idchain: {
       provider: function () {
         return new HDWalletProvider(
-          mnemonic.getMnemonic(),
-          "http://127.0.0.1:8546/"
+          private.mnemonic,
+          "https://idchain.one/rpc/"
         );
+      },
+      network_id: 74,
+    },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(private.mnemonic, private.kovanEndpoint);
+      },
+      network_id: 42,
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(private.mnemonic, private.ropstenEndpoint);
+      },
+      network_id: 3,
+    },
+    development9545: {
+      provider: function () {
+        return new HDWalletProvider(private.mnemonic, "http://127.0.0.1:9545/");
       },
       network_id: "*",
     },
   },
   compilers: {
     solc: {
-      version: "0.5.16",
-      optimisation: false,
+      settings: {
+        version: "0.5.16",
+        optimizer: {
+          enabled: true,
+        },
+      },
     },
   },
 };
