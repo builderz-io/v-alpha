f4e415b0bd56fbbc821a5f6ac3f823f49fc48afb
Marc Griffiths <grifma@protonmail.com>
2020-08-13 19:02:35 +0100

    Including build directory. Deployed to network: devvps

--- smart-contract/truffle-config.js
+++ smart-contract/truffle-config.js
@@ -9,8 +9,22 @@ module.exports = {
       network_id: "*",
       websockets: true,
     },
+    devvps: {
+      provider: function () {
+        return new HDWalletProvider(
+          private.devvpsKeys,
+          "http://161.97.97.238:8547",
+          0,
+          1
+        );
+      },
+      network_id: "1337",
+      // host: "161.97.97.238",
+      // port: 8547,
+      // websockets: true,
+    },
     idchain: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(
           private.mnemonic,
           "https://idchain.one/rpc/"
@@ -19,26 +33,26 @@ module.exports = {
       network_id: 74,
     },
     kovan: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(private.mnemonic, private.kovanEndpoint);
       },
       network_id: 42,
     },
     rinkeby: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(private.mnemonic, private.rinkebyEndpoint);
       },
       network_id: 4,
       gas: 4612388,
     },
     ropsten: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(private.mnemonic, private.ropstenEndpoint);
       },
       network_id: 3,
     },
     development9545: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(private.mnemonic, "http://127.0.0.1:9545/");
       },
       network_id: "*",
