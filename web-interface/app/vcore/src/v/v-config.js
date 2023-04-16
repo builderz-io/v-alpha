f2cd0f8fe7a677c967b249336bf3dbbb1e5926f0
Philipe Achille Villiers <philipeachille@gmail.com>
2023-04-16 15:48:19 +0200

    Version 3.5.5

--- web-interface/app/vcore/src/v/v-config.js
+++ web-interface/app/vcore/src/v/v-config.js
@@ -55,8 +55,8 @@ const VConfig = ( function() { // eslint-disable-line no-unused-vars
 
   const settings = {
 
-    appVersion: 'Alpha 3.5.4',
-    uploadVersion: '9',
+    appVersion: 'Alpha 3.5.5',
+    uploadVersion: '10',
 
     entityLedger: VNetworkInit.entityLedger,
     chatLedger: VNetworkInit.chatLedger,
