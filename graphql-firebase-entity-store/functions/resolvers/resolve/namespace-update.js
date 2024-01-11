db6995ccc100e2a7cb7e48648bcc18d3e602c582
philipeachille <philipeachille@gmail.com>
2024-01-11 02:05:19 +0100

    [ADD] expose geo data as per privacy setting

--- graphql-firebase-entity-store/functions/resolvers/resolve/namespace-update.js
+++ graphql-firebase-entity-store/functions/resolvers/resolve/namespace-update.js
@@ -6,6 +6,7 @@ const collI = global.db.collI;
 const castObjectPaths = require( './utils/cast-object-paths' );
 const trackProfileFields = require( './utils/track-profile-fields' );
 const setNetwork = require( './utils/set-network' );
+const { decrypt } = require( '../../resources/crypt' );
 
 module.exports = async ( context, data, objToUpdate, coll ) => {
 
@@ -16,12 +17,12 @@ module.exports = async ( context, data, objToUpdate, coll ) => {
   await require( '../validate/validate' )( context, data, objToUpdate );
 
   /** Generate geohash */
-  if (
-    objToUpdate.b.includes( '/p' )
-    && data.n
-  ) {
-    data.n.b = require( 'geofire-common' ).geohashForLocation( [ data.n.a[1], data.n.a[0] ] );
-  }
+  // if (
+  //   objToUpdate.b.includes( '/p' )
+  //   && data.n
+  // ) {
+  //   data.n.b = require( 'geofire-common' ).geohashForLocation( [ data.n.a[1], data.n.a[0] ] );
+  // }
 
   /** Copy tinyImg and thumb to profile db */
   if ( data.o ) {
@@ -40,6 +41,24 @@ module.exports = async ( context, data, objToUpdate, coll ) => {
 
     /** Track profile fields in entity db */
     trackProfileFields( objToUpdate.d, data );
+
+    /**
+     * Expose an encrypted geo location as per privacy setting
+     */
+
+    if (
+      ( data.f || data.f === 0 )
+      && objToUpdate.n && objToUpdate.n.d
+    ) {
+      data.n = {};
+      if ( data.f != 1 ) {
+        const geo = JSON.parse( decrypt( JSON.parse( objToUpdate.n.d ) ) );
+        data.n.a = geo;
+      }
+      else if ( data.f == 1 ) {
+        data.n.a = null;
+      }
+    }
   }
 
   const fields = castObjectPaths( data );
@@ -51,18 +70,20 @@ module.exports = async ( context, data, objToUpdate, coll ) => {
   objToUpdate.b.includes( '/e' ) ? delete fields.c : null;
 
   /**
-   * Update single fields in collection and
-   * set privacy field in image db also
+   * copy privacy field setting to image db
    */
-  await new Promise( resolve => {
-    coll.child( data.a ).update( fields, () => resolve( data ) );
-  } );
-
   if ( data.f || data.f === 0 ) {
     await new Promise( resolve => {
       collI.child( data.a ).update( fields, () => resolve( data ) );
     } );
   }
+
+  /**
+   * Update single fields in collection
+   */
+  await new Promise( resolve => {
+    coll.child( data.a ).update( fields, () => resolve( data ) );
+  } );
 
   /** Update the network cluster and point-cache */
   setNetwork( context );
