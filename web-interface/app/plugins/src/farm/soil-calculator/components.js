7dc4e88d0f08f13383499408e2d5052afed8ba75
Philipe Achille Villiers <philipeachille@gmail.com>
2024-09-12 10:29:20 +0200

    feature: multiple fertilizers
    Handling up to 5 fertilizers. Their values are being saved to the DB and
    each group is being toggled by filling up the previous first.

--- web-interface/app/plugins/src/farm/soil-calculator/components.js
+++ web-interface/app/plugins/src/farm/soil-calculator/components.js
@@ -168,6 +168,17 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
       'border-radius': '50px',
 
     },
+
+    // 'fertilizers .s-calc-input-wrapper:first-of-type': {
+    //   'font-weight': '700',
+    //   'font-family': 'IBM Plex Bold',
+    // },
+
+    'fertilizers .s-calc-input-wrapper:not(:first-of-type)': {
+      'padding': '0 0 1rem 1rem',
+      'margin': '0',
+      'border-left': '1px solid black',
+    },
   } );
 
   /* ============== user interface strings ============== */
@@ -205,6 +216,12 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
 
       /* is not the case when loading data from db */
       run = setDatapoint( e ); /* save to db */
+    }
+
+    if ( !e ) {
+
+      /* checking if on profile load, we need to toggle any fertilizer groups */
+      toggleFertliziersGroups( 'CROP-1' );
     }
 
     if ( !run ) { return }
@@ -451,6 +468,25 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
     V.getNode( '.s-calc-summary' ).append( summaryTable( sequence ) );
   }
 
+  function toggleFertliziersGroups( formName ) {
+    const _ = document.forms[formName].elements;
+    for ( let i = 1; i <= 5; ++i ) {
+      const nextFertilizerGroupIdx = i+1;
+      const groupElement = document.querySelector( `.FTLZ_F${nextFertilizerGroupIdx}` );
+
+      const shouldDisplayTheNextFertilizerGroup = _[`FTLZ_F${i}_ID`].value !== '5000'
+        && !!_[`FTLZ_F${i}_QTY`].value
+        && !!_[`FTLZ_F${i}_DATE`].value;
+
+      if ( shouldDisplayTheNextFertilizerGroup && nextFertilizerGroupIdx <= 5 && groupElement ) {
+        groupElement.classList.toggle( 'hidden', false );
+      }
+      else if ( nextFertilizerGroupIdx <= 5 && groupElement &&  _[`FTLZ_F${i}_ID`].value === '5000' && !_[`FTLZ_F${i}_QTY`].value ) {
+        groupElement.classList.toggle( 'hidden', true );
+      }
+    }
+  }
+
   function setDatapoint( e ) {
 
     const formName = e.target.closest( 'form' ).getAttribute( 'name' );
@@ -460,6 +496,10 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
       : formName.replace( 'CROP-', '' );
 
     const newDatapoint = getFormData( formName );
+
+    if ( e.target.name.includes( 'FTLZ' ) ) {
+      toggleFertliziersGroups( formName );
+    }
 
     if (
       newDatapoint === -30
@@ -531,8 +571,13 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
 
       __.CROP.ID = Number( _.CROP_ID.value );
 
-      __.FTLZ.ORG.ID = Number( _.FTLZ_ORG_ID.value );
-      __.FTLZ.ORG.QTY = Number( _.FTLZ_ORG_QTY.value );
+      // __.FTLZ.ORG.ID = Number( _.FTLZ_ORG_ID.value );
+      // __.FTLZ.ORG.QTY = Number( _.FTLZ_ORG_QTY.value );
+      for ( let i = 1; i <= 5; ++i ) {
+        __.FTLZ[`F${i}`].ID = Number( _[`FTLZ_F${i}_ID`].value );
+        __.FTLZ[`F${i}`].QTY = Number( _[`FTLZ_F${i}_QTY`].value );
+        __.FTLZ[`F${i}`].DATE = _[`FTLZ_F${i}_DATE`].value;
+      }
 
       __.BMASS.MP.QTY = Number( _.BMASS_MP_QTY.value );
       __.BMASS.MP.HVST = ( _.BMASS_MP_HVST.value === 'true' );
@@ -1227,6 +1272,31 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
         ID: inputDropID,
       },
       FTLZ: {
+        F1: {
+          ID: inputDropID,
+          QTY: inputNum,
+          DATE: inputDate,
+        },
+        F2: {
+          ID: inputDropID,
+          QTY: inputNum,
+          DATE: inputDate,
+        },
+        F3: {
+          ID: inputDropID,
+          QTY: inputNum,
+          DATE: inputDate,
+        },
+        F4: {
+          ID: inputDropID,
+          QTY: inputNum,
+          DATE: inputDate,
+        },
+        F5: {
+          ID: inputDropID,
+          QTY: inputNum,
+          DATE: inputDate,
+        },
         ORG: {
           ID: inputDropID,
           QTY: inputNum,
@@ -1265,14 +1335,14 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
     } );
 
     const fieldGroup = ( section, field ) => V.cN( {
-      c: 's-calc-form__field-group',
+      c: `s-calc-form__field-group ${section}_${field}`,
       h: [
         {
           c: 's-calc-form__field-group-title font-bold',
           h: SoilCalculator.getFieldString( section + '_' + field, locale ),
         },
         {
-          c: 's-calc-form__field-group-fields',
+          c: `s-calc-form__field-group-fields ${section === 'FTLZ' ? 'fertilizers' : ''}`,
           h: Object.keys( data[section][field] )
             .map( subField => templates[section][field][subField]( data[section][field][subField], castFlatFieldTitle( section, field, subField ) ) ),
         },
@@ -1295,10 +1365,17 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
           {
             c: 's-calc-form__section-fields',
             h: Object.keys( data[section] )
-              .map( field => typeof data[section][field] == 'object'
-                ? fieldGroup( section, field )
-                : fieldSingle( section, field ),
-              ),
+              .map( field => {
+                const group = typeof data[section][field] == 'object'
+                  ? fieldGroup( section, field )
+                  : fieldSingle( section, field );
+
+                if ( section === 'FTLZ' && field !== 'F1' ) {
+                  group.classList.add( 'hidden' );
+                }
+
+                return group;
+              } ),
           },
         ],
       } ) ),
@@ -1350,7 +1427,7 @@ const SoilCalculatorComponents = ( function() { // eslint-disable-line no-unused
                 : (
                   SoilCalculator.getCropName( item.CROP.ID, locale )
                   + ' & '
-                  + SoilCalculator.getFertilizerName( item.FTLZ.ORG.ID, locale )
+                  + SoilCalculator.getFertilizerName( item.FTLZ.F1.ID, locale ) // @TODO(fertilizers): handle the multiple fertilizers?
                 ),
             },
           ],
