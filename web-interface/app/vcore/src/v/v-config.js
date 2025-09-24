429fef594e311012bf16c94d4f6dcbc415b9d6af
philipeachille <philipeachille@gmail.com>
2025-09-24 17:49:30 +0200

    Version 3.7.5

--- web-interface/app/vcore/src/v/v-config.js
+++ web-interface/app/vcore/src/v/v-config.js
@@ -56,7 +56,7 @@ const VConfig = ( function() { // eslint-disable-line no-unused-vars
 
   const settings = {
 
-    appVersion: 'Alpha 3.7.4',
+    appVersion: 'Alpha 3.7.5',
     uploadVersion: '1',
 
     entityLedger: VNetworkInit.entityLedger,
